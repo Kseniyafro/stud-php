a0e220c54b625f70b49cccc91b341e62844339f8
Kseniyafro <ksufrolova2006@yandex.ru>
2025-02-08 12:06:14 +0300

    creat cat object

---
+++ web-server-programming/project/index.php
@@ -0,0 +1,23 @@
+<?php
+   class Cat{
+      private $name;
+      public $color;
+      public $weight;
+      
+      function __construct(string $name, string $color, int $weight){
+         $this->name = $name;
+         $this->color = $color;
+         $this->weight = $weight;
+      }
+
+      function setName(string $name){
+         $this->name = $name;
+      }
+      function getName(){
+         return $this->name;
+      }
+   }
+
+   $cat1 = new Cat('murka', 'black', 3);
+   echo $cat1->getName().'<BR';
+   var_dump($cat1);
